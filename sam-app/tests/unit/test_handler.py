45fd64da5e7f64d007c2838fe6c9947abae1e39d
Denis <denisstortisilva@gmail.com>
2019-05-29 19:10:39 +1000

    fix

--- sam-app/tests/unit/test_handler.py
+++ sam-app/tests/unit/test_handler.py
@@ -1,7 +1,7 @@
 import json
 import sys
 import requests
-sys.path.append('c:\\src\\sampython\\sam-app\\hello_world')
+#sys.path.append('c:\\src\\sampython\\sam-app\\hello_world')
 sys.path.append('.\\hello_world')  # ESSENTIAL TO IMPORT MESSAGE MODULE
 
 import pytest
